d4262339e9601cc49acd9c493eca8e023a1f87b9
Молоствов Игорь <Jaiko876@gmail.com>
2020-08-08 14:05:00 +0300

    Homework4

---
+++ Homework4/MyArray.cs
@@ -0,0 +1,69 @@
+﻿// 3. a Дописать класс для работы с одномерным массивом.
+namespace Homework4
+{
+    public class MyArray
+    {
+        private int[] arr;
+
+        public int[] Arr => arr;
+
+        private int size;
+
+        public MyArray(int size, int startNum, int step)
+        {
+            arr = new int[size];
+            arr[0] = startNum;
+            for (int i = 1; i < size; i++)
+            {
+                arr[i] = startNum + step;
+            }
+
+            this.size = size;
+        }
+
+        public int Sum
+        {
+            get
+            {
+                int sum = 0;
+                foreach (var num in arr)
+                {
+                    sum += num;
+                }
+                return sum;
+            }
+        }
+
+        public int[] Inverse()
+        {
+            int[] temp = new int[arr.Length];
+            int k = 0;
+            for (int i = arr.Length - 1; i >= 0; i--)
+            {
+                temp[k++] = arr[i];
+            }
+            return temp;
+        }
+
+        public void Multy(int multiplier)
+        {
+            int[] temp = new int[arr.Length];
+            int i = 0;
+            foreach (var num in arr)
+            {
+                temp[i] = arr[i] * multiplier;
+                i++;
+            }
+
+            arr = temp;
+        }
+
+        public int MaxCount
+        {
+            get
+            {
+                return size;
+            }
+        }
+    }
+}
